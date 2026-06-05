96b423100bec382be435157ce7328cd7aeb84f85
tatsukiyama <101154444+tatsukiyama@users.noreply.github.com>
2026-06-05 13:16:45 +0900

    文字化け

--- Teams-Presence-Manager.ps1
+++ Teams-Presence-Manager.ps1
@@ -79,6 +79,7 @@ try {
             $textContent | Out-File -FilePath $savePath -Encoding UTF8
         }
         #>
+        <#
         foreach ($item in $downloadList) {
             $fileUrl = "$baseUrl/$($item.Remote)"
             $savePath = Join-Path $workDir $item.Local
@@ -89,6 +90,18 @@ try {
             # ② PowerShellが絶対に読み間違えないように「BOM付きUTF-8」として上書き保存
             $safeText = [System.IO.File]::ReadAllText($savePath, [System.Text.Encoding]::UTF8)
             [System.IO.File]::WriteAllText($savePath, $safeText, [System.Text.Encoding]::UTF8)
+        }
+        #>
+        foreach ($item in $downloadList) {
+            $fileUrl = "$baseUrl/$($item.Remote)"
+            $savePath = Join-Path $workDir $item.Local
+            
+            # ① いったんファイルとしてそのままダウンロード
+            Invoke-WebRequest -Uri $fileUrl -Headers $headers -OutFile $savePath
+            
+            # ② UTF-8(BOMなし)として読み込み、PowerShellが絶対に間違えない「BOM付きUTF-8」として保存し直す
+            $text = [System.IO.File]::ReadAllText($savePath, [System.Text.Encoding]::UTF8)
+            $text | Out-File -FilePath $savePath -Encoding UTF8
         }
 
         $mainPath = Join-Path $workDir "Main-Controller.ps1"
